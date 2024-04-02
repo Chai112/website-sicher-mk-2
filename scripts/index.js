736006f1a1154982ce58a3e0761f57a8a6fc3ce4
chaidhat <chaimongkol@ucla.edu>
2024-04-01 22:03:56 -0700

    implement carousel to suggested courses

--- scripts/index.js
+++ scripts/index.js
@@ -13,14 +13,30 @@ async function populateLectureList () {
     let data = await fetchServer({action: "sicher_getTopLectures"});
 
     for (let i = 0; i < data.data.length; i++) {
-        const html = `<div class="col">
+        const listHtml = `
+        <div class="col">
             <div class="tile news-tile shadow animation-up" id="news-3">
                 <h2>${decodeURI(data.data[i].lectureName)}</h2>
                 <p>${decodeURI(data.data[i].data)}...</p>
-                <p><b>READ MORE</b></p>
+                <span class="material-symbols-outlined">
+                arrow_forward
+                </span>
             </div>
         </div>`;
-        $('#lecture-list').append(html);
+        $('#lecture-list').append(listHtml);
+        const carouselHtml = `
+        <div class="client carousel-item${i === 0 ? " active" : ""}">
+            <div class="inner">
+                <div class="tile news-tile shadow animation-up" id="news-3">
+                    <h2>${decodeURI(data.data[i].lectureName)}</h2>
+                    <p>${decodeURI(data.data[i].data)}...</p>
+                    <span class="material-symbols-outlined">
+                    arrow_forward
+                    </span>
+                </div>
+            </div>
+        </div>`;
+        $('#lecture-carousel-list').append(carouselHtml);
     }
 }
 
