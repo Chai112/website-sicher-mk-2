5bfa035eba920372d550713bb15c333d72a29064
chaidhat <chaimongkol@ucla.edu>
2024-04-03 18:21:13 -0700

    add fuzzy find

--- scripts/training.js
+++ scripts/training.js
@@ -1,3 +1,5 @@
+import Fuse from 'https://cdn.jsdelivr.net/npm/fuse.js@7.0.0/dist/fuse.mjs'
+
 async function fetchServer(data) {
     const response = await fetch("https://server-singapore.scholarity.io/", {
         method: "POST", // or 'PUT'
@@ -15,17 +17,40 @@ async function goToLecture(sicherLectureId) {
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-async function populateLectureList () {
+let globalLectureData = [];
 
+async function getLectures() {
     const data = await fetchServer({action: "sicher_getLectures"});
+    globalLectureData = data.data;
+    populateLectureList(globalLectureData);
+}
 
+async function updateLectureList() {
+    const searchTerm = $("#searchbar").val();
+    if (searchTerm === "") {
+        populateLectureList(globalLectureData);
+        return;
+    } 
+
+    const fuse = new Fuse(globalLectureData, {
+        keys: ['lectureName', 'instructors', 'dateHeld']
+    })
+    let lectureData = [];
+    const output = fuse.search(searchTerm);
+    for (let i = 0; i < output.length; i++) {
+        lectureData.push(output[i].item);
+    }
+    populateLectureList(lectureData);
+}
+
+function populateLectureList (lectureData) {
     $("#lecture-list").empty();
-    for (let i = 0; i < data.data.length; i++) {
+    for (let i = 0; i < lectureData.length; i++) {
         const listHtml = `
         <div class="col">
             <div class="tile news-tile shadow" id="lecture-list-${i}">
-                <h2>${decodeURI(data.data[i].lectureName)}</h2>
-                <p>${decodeURI(data.data[i].data)}...</p>
+                <h2>${decodeURI(lectureData[i].lectureName)}</h2>
+                <p>${decodeURI(lectureData[i].data)}...</p>
                 <span class="material-symbols-outlined">
                 arrow_forward
                 </span>
@@ -33,19 +58,22 @@ async function populateLectureList () {
         </div>`;
         $('#lecture-list').append(listHtml);
         $(`#lecture-list-${i}`).click(function () {
-            goToLecture(data.data[i].sicherLectureId);
+            goToLecture(lectureData[i].sicherLectureId);
         });
     }
-    for (let i = data.data.length % 3; i < 3; i++) {
+    for (let i = lectureData.length % 3; i < 3 && i != 0; i++) {
         const listHtml = `
         <div class="col">
         <div></div>
         </div>`;
-        console.log(i);
         $('#lecture-list').append(listHtml);
     }
 }
 
 $(document).ready(function(){
-    populateLectureList();
+    getLectures();
+});
+
+$('#searchbar').on('input',function(e){
+    updateLectureList();
 });
