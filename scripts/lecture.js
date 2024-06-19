883f3298c63c9ba4d3bbcf4642d977bd0b2d4585
chaidhat <chaimongkol@ucla.edu>
2024-06-19 11:41:24 +0700

    add trainings frontend

--- scripts/lecture.js
+++ scripts/lecture.js
@@ -18,6 +18,38 @@ async function loadLecture() {
     $("#lecture-title").text(decodeURI(data.data[0].lectureName));
     $("#lecture-content").empty();
     const lectureData = JSON.parse(decodeURI(data.data[0].data));
+
+    let includeNotes = false;
+    for (let i = 0; i < lectureData.trainings.length; i++) {
+        if (lectureData.trainings[i].notes !== "") {
+            includeNotes = true; 
+            break;
+        }
+    }
+    // training data
+    let html = `<table>
+        <tr>
+            <th>Date</th>
+            <th>Venue</th> 
+            <th class="book-button-td"></th>
+            ${includeNotes ? '<th>Notes</th>' : ''}
+        </tr>`;
+
+    for (let i = 0; i < lectureData.trainings.length; i++) {
+        html += `<tr>
+            <td>${lectureData.trainings[i].dateStart}</td>
+            <td>${lectureData.trainings[i].venue}</td>
+            <td class="book-button-td">
+                <button class="button" onclick="window.location.href='/training.html'">Book</button>
+            </td>
+            ${includeNotes ? "<td>" + lectureData.trainings[i].notes + "</td>" : ""}
+        </tr>`;
+    };
+
+    html += `</table>`;
+    $('#lecture-content').append(html);
+
+    // syllabus data
     for (let i = 0; i < lectureData.data.length; i++) {
         const sectionName = lectureData.data[i].sectionName;
         const sectionDescription = lectureData.data[i].sectionDescription;
